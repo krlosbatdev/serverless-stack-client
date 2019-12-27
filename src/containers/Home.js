0be94f0eefb6719f49444ce18438e55d4736b9ae
Kar <krlosbatdev@gmail.com>
2019-12-27 11:30:08 -0500

    Change app name

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -55,7 +55,7 @@ export default function Home(props) {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
+        <h1>Note it</h1>
         <p>A simple note taking app</p>
         <div>
           <Link to="/login" className="btn btn-info btn-lg">
